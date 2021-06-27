ba700db532c66c30b5678e5432ab87d4a07721bc
Pooya-Y <54044959+Pooya-Y@users.noreply.github.com>
2021-06-27 14:22:01 +0430

    refactor

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,6 @@
-const {auth, isAdmin} = require("../middlewares/jwt");
+const {auth} = require("../middlewares/jwt");
 const express = require('express');
 const router = express.Router();
-const multer = require("multer");
-
 const {
     getUserPostsById,
     getTimeline,
@@ -11,26 +9,7 @@ const {
     unlikePost,
     newMention
 } = require("../controllers/posts");
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads')
-    },
-    filename: function (req, file, cb) {
-      const fileName = file.originalname.split(" ").join("-");
-      cb(null, Date.now().toString()+"-"+fileName)
-    },
-});
-const upload = multer({ 
-    storage: storage ,
-    limits:{fileSize: 1024*1024*10},
-    fileFilter: function (req, file, cb) {
-        if(file.mimetype !== 'image/png' && file.mimetype !== 'image/jpg' && file.mimetype !== 'image/jpeg') {
-            cb(new Error("Wrong file type"), false);
-        }
-        cb(null, true);
-    },
-})
+const {upload} = require("../controllers/multer");
 
 router.get("/user/:id", getUserPostsById);
 
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,6 @@
-const sha256 = require('sha256');
-const mongoose =require("mongoose");
 const {auth, isAdmin} = require("../middlewares/jwt");
 const express = require('express');
 const router = express.Router();
-const jwt = require("jsonwebtoken");
-const User = require('../models/user');
-const multer = require("multer");
 const {
     getUsers,
     getUserById,
@@ -15,27 +10,7 @@ const {
     followUser,
     unfollowUser
 } = require("../controllers/users");
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads')
-    },
-    filename: function (req, file, cb) {
-      const fileName = file.originalname.split(" ").join("-");
-      cb(null, Date.now().toString()+"-"+fileName)
-    },
-});
-   
-const upload = multer({ 
-    storage: storage ,
-    limits:{fileSize: 1024*1024*10},
-    fileFilter: function (req, file, cb) {
-        if(file.mimetype !== 'image/png' && file.mimetype !== 'image/jpg' && file.mimetype !== 'image/jpeg') {
-            cb(new Error("Wrong file type"), false);
-        }
-        cb(null, true);
-    },
-})
+const {upload} = require("../controllers/multer");
 
 router.get('', getUsers);
 
